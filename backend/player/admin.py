e5b673e51054168e1c4bcba9c955ea37d2071ffb
NatLee <natlee.work@gmail.com>
2023-01-22 18:06:19 +0800

    [update] add model admin page

--- backend/player/admin.py
+++ backend/player/admin.py
@@ -1,3 +1,51 @@
+import datetime
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter, NumericRangeFilter
 
-# Register your models here.
+from simple_history.admin import SimpleHistoryAdmin
+
+from player.models import (
+    Playlist,
+    PlaylistOrderHistory,
+)
+
+def get_last_week():
+    return datetime.date.today()-datetime.timedelta(days=7)
+
+@admin.register(Playlist)
+class PlaylistAdmin(SimpleHistoryAdmin):
+    list_display = (
+        "id",
+        "song_name",
+        "url",
+        "last_played_at",
+        "created_at",
+    )
+    list_filter = (
+        ('created_at', DateRangeFilter),
+    )
+    search_fields = ["song_name", "url"]
+
+    def get_rangefilter_created_at_default(self, request):
+        return (get_last_week, datetime.date.today)
+
+
+@admin.register(PlaylistOrderHistory)
+class PlaylistOrderHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "playlist",
+        "user",
+        "played",
+        "created_at",
+    )
+    list_filter = (
+        ('created_at', DateRangeFilter),
+    )
+    search_fields = ["playlist__song_name", "playlist__url", "user"]
+
+    def get_rangefilter_created_at_default(self, request):
+        return (get_last_week, datetime.date.today)
+
+
+
